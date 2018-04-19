f4b61418e786e7283a5d6aaf6d5d1e253bdc6e56
wcalvert88 <wcalvert88@gmail.com>
2018-04-19 16:54:55 -0400

    added a find this query method that saves a lot of code in other places

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,19 +3,27 @@
 class User {
 
     public static function find_all_users() {
-        global $database;
-        $result_set = $database->query("SELECT * FROM users");
-        return $result_set;
+        // global $database;
+        // $result_set = $database->query("SELECT * FROM users");
+        // return $result_set;
+
+        return self::find_this_query("SELECT * FROM users");
     }
 
     public static function find_user_by_id($id) {
         global $database;
-        $result_set = $database->query("SELECT * FROM users WHERE id = $id LIMIT 1");
+         $result_set = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
         $found_user = mysqli_fetch_array($result_set);
         return $found_user;
+
     }
 
+    public static function find_this_query($sql) {
+        global $database;
 
+        $result_set = $database->query($sql);
+        return $result_set;
+    }
 
 }
 
