de1d731fc1b69f4075f5c3cdfc754a30c3ec05fc
wcalvert88 <wcalvert88@gmail.com>
2018-04-19 10:34:43 -0400

    changed how everything in the database was called

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -11,28 +11,35 @@ class Database {
 
 
     public function open_db_connection() {
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if (mysqli_connect_errno()){
-            die("Database connection failed" . mysqli_error());
+        // $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        if ($this->connection->connect_errno) {
+            die("Database connection failed" . $this->connection->connect_error);
         }
     }
 
     public function query($sql) {
-        $result = mysqli_query($this->connection, $sql);
-
+        $result = $this->connection->query($sql);
+        $this->confirm_query($result);
         return $result;
     }
 
     private function confirm_query($result){
         if(!$result) {
-            die("Query Failed" . mysqli_error($connection));
+            die("Query Failed" . $this->connection->connect_error);
         }
 
     }
 
     public function escape($string) {
-        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        $escaped_string = $this->connection->real_escape_string($string);
         return $escaped_string;
+    }
+
+    public function the_insert_id() {
+        return $this->connection->insert_id;
     }
 
 }
