9c2813ea4be4ecf9b7fa9231c5cb13022c56d553
wcalvert88 <wcalvert88@gmail.com>
2018-04-19 09:54:00 -0400

    added a database class

---
+++ admin/includes/database.php
@@ -0,0 +1,21 @@
+<?php
+require_once("new_config.php");
+
+class Database {
+
+    private $connection;
+    public function open_db_connection() {
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if (mysqli_connect_errno()){
+            die("Database connection failed" . mysqli_error());
+        }
+    }
+
+}
+
+$database = new Database();
+
+$database->open_db_connection();
+
+
+?>
