103d888a5380178fad91e63b0eaf153bb77e5bba
wcalvert88 <wcalvert88@gmail.com>
2018-04-19 09:59:19 -0400

    made database automatically connect

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -3,7 +3,13 @@ require_once("new_config.php");
 
 class Database {
 
-    private $connection;
+    public $connection;
+
+    function __construct() {
+        $this->open_db_connection();
+    }
+
+
     public function open_db_connection() {
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         if (mysqli_connect_errno()){
@@ -15,7 +21,7 @@ class Database {
 
 $database = new Database();
 
-$database->open_db_connection();
+
 
 
 ?>
