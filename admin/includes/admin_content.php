213b204d8d7e4443705d85af3daaf5750ff8fa19
wcalvert88 <wcalvert88@gmail.com>
2018-04-19 11:13:35 -0400

    add find all users method to user class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,10 +9,15 @@
             </h1>
             
             <?php 
-            $sql = "SELECT * FROM users WHERE id=1";
-            $result = $database->query($sql);
-            $user_found= mysqli_fetch_array($result);
-            echo $user_found['username'];
+            
+            $user = new User();
+            $result_set = $user->find_all_users();
+            
+            while($row = mysqli_fetch_array($result_set)) {
+                echo $row['username'] . "<br>";
+                
+            }
+
             ?>
             <ol class="breadcrumb">
                 <li>
