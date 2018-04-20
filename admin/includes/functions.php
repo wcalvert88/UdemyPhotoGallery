af7eca854d9a7a0d2626ee2b30dcf55b56c15128
wcalvert88 <wcalvert88@gmail.com>
2018-04-20 12:50:21 -0400

    autoloader is depreciated so I am using a new function classAutoloader and used spl_autoload_register

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,18 +1,34 @@
 <?php 
 
-function __autoload($class) {
+// function __autoload($class) {
+
+//     $class = strtolower($class);
+//     $path = "includes/{$class}.php";
+
+//     if (file_exists($path)) {
+//         require_once($path);
+//     } else {
+//         die("This file name {$class}.php was not found");
+//     }
+// }
+
+function classAutoloader($class) {
 
     $class = strtolower($class);
     $path = "includes/{$class}.php";
 
-    if (file_exists($path)) {
-        require_once($path);
-    } else {
-        die("This file name {$class}.php was not found");
+
+    if(is_file($path) && !class_exists($class)) {
+        include $path;
     }
+    // if (file_exists($path)) {
+    //     require_once($path);
+    // } else {
+    //     die("This file name {$class}.php was not found");
+    // }
 }
 
-
+spl_autoload_register('classAutoLoader');
 
 
 
