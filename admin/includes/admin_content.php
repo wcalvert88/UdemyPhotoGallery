8b47f64ae4976528a70ac79291e4e5d003ba2e13
wcalvert88 <wcalvert88@gmail.com>
2018-04-20 09:27:48 -0400

    updated find 1 user method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -22,10 +22,12 @@
             // $user = User::instantiation($found_user);
             // echo $user->id;
 
-            $users = User::find_all_users();
-            foreach($users as $user) {
-                echo $user->username . "<br>";
-            }
+            // $users = User::find_all_users();
+            // foreach($users as $user) {
+            //     echo $user->username . "<br>";
+            // }
+            $found_user = User::find_user_by_id(1);
+            echo $found_user->username;
             ?>
             <ol class="breadcrumb">
                 <li>
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -18,8 +18,13 @@ class User {
 
     public static function find_user_by_id($id) {
         global $database;
-         $result_set = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
-        $found_user = mysqli_fetch_array($result_set);
+        $result_array = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
+        return !empty($result_array) ? array_shift($result_array) : false;
+        // if(!empty($result_array)) {
+        //     $first_item = array_shift($result_array);
+        // } else {
+        //     return false;
+        // }
         return $found_user;
 
     }
