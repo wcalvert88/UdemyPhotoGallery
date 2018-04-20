531f5c1225f030a1270822c43eeff5b384c36602
wcalvert88 <wcalvert88@gmail.com>
2018-04-20 13:03:12 -0400

    created a check_login method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -11,6 +11,17 @@ class Session {
     }
 
 
+    private function check_login() {
+        if(isset($_SESSION['user_id'])) {
+            $this->user_id = $_SESSION['user_id'];
+            $this->signed_in = true;
+        } else {
+            unset($this->user_id);
+            $this->signed_in = false;
+        }
+    }
+
+
 }
 
 $session = new Session();
