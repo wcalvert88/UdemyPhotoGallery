520763735f07a3a170fc35b7986a9a767084291f
wcalvert88 <wcalvert88@gmail.com>
2018-04-20 13:42:01 -0400

    created login page

---
+++ admin/includes/login.php
@@ -0,0 +1,28 @@
+<?php require_once("init.php");
+
+if($session->is_signed_in()){
+    redirect("../index.php");
+
+}
+
+if(isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    /// Method to check database user
+
+
+
+    if($user_found) {
+        $session->login($user_found);
+        redirect("../index.php");
+    } else {
+        $message = "Your username or password is incorrect";
+    }
+} else {
+    $username = "";
+    $password = "";
+}
+
+
+?>
