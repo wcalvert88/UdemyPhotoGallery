ab4081fa6549ae571d7e8e3217ed43411d5e8f59
wcalvert88 <wcalvert88@gmail.com>
2018-04-21 09:57:27 -0400

    made upload any kind of file form

---
+++ basic_files/upload.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="file_upload"><br>
+
+        <input type="submit" name="submit">
+        
+    </form>
+</body>
+</html>
