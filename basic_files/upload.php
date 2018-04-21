e2bd8ecaece51ca72794ecc0a60b8b576cd8b9f5
wcalvert88 <wcalvert88@gmail.com>
2018-04-21 10:21:59 -0400

    put in error messages for files

--- basic_files/upload.php
+++ basic_files/upload.php
@@ -1,9 +1,28 @@
 <?php 
+
+if(isset($_POST['submit'])) {
 echo "<pre>";
+
 print_r($_FILES['file_upload']);
 
 echo "<pre>";
 
+$upload_errors = array(
+
+    UPLOAD_ERR_OK => "There is no error",
+    UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive in php.ini",
+    UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.",
+    UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded.",
+    UPLOAD_ERR_NO_FILE => "No file was uploaded.",
+    UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder.",
+    UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk.",
+    UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file upload."
+);
+
+$error = $_FILES['file_upload']['error'];
+
+$message = $upload_errors[$error];
+}
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +37,14 @@ echo "<pre>";
 </head>
 <body>
     <form action="upload.php" method="post" enctype="multipart/form-data">
+        <h2>
+        <?php 
+        if(!empty($upload_errors)) {
+            echo $message;
+        }
+        ?>
+        </h2>
+
         <input type="file" name="file_upload"><br>
 
         <input type="submit" name="submit">
