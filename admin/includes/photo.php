93a7ce059c7089be704d5b2f22b253ee8173b376
wcalvert88 <wcalvert88@gmail.com>
2018-04-22 14:36:51 -0400

    started building photo class

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -2,12 +2,18 @@
 
 
     class Photo extends Db_object {
+        protected static $db_table = "photos";
+        protected static $db_table_fields = array('title', 'description', 'filename', 'type', 'size');
+        public $photo_id;
+        public $title;
+        public $description;
+        public $filename;
+        public $type;
+        public $size;
 
 
 
 
-
-        
     }
 
 
