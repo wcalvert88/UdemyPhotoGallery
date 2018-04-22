e45130c8fedf0c12eab3b9ed4682901ce2dcd7e5
wcalvert88 <wcalvert88@gmail.com>
2018-04-22 16:19:36 -0400

    created form to upload pictures

--- admin/upload.php
+++ admin/upload.php
@@ -1,4 +1,8 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/header.php");
+if(!$session->is_signed_in()) {
+    redirect("login.php");
+}
+?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -27,14 +31,19 @@
                 Upload
                 <small>Subheading</small>
             </h1>
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> Blank Page
-                </li>
-            </ol>
+            <div class="col-md-6">
+                <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="file" name="file_upload">
+                    </div>
+                    <input type="submit" name="submit">
+                
+                
+                </form>
+            </div>
         </div>
     </div>
     <!-- /.row -->
