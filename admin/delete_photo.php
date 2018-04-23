147d4936b4dac1e47badcf98c07610790df3fbec
wcalvert88 <wcalvert88@gmail.com>
2018-04-23 15:38:50 -0400

    set up delete_photo.php page and fixed links in photos

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -2,5 +2,16 @@
 if(!$session->is_signed_in()) {
     redirect("login.php");
 }
-echo "it works";
+
+if(empty($_GET['photo_id'])) {
+    redirect("photos.php");
+}
+
+$photo = Photo::find_by_id($_GET['photo_id']);
+
+if($photo) {
+    $photo->delete_photo();
+} else {
+    redirect("photos.php");
+}
 ?>
--- admin/photos.php
+++ admin/photos.php
@@ -51,9 +51,9 @@ $photos = Photo::find_all();
                             <td>
                                 <img src="<?php echo $photo->picture_path(); ?>" alt="" width="200px">
                                 <div class="pictures_link">
-                                    <a href="delete_photo.php/?id=<?php echo $photo->photo_id;?>">Delete</a>
-                                    <a href="edit_photo.php/?id=<?php echo $photo->photo_id; ?>">Edit</a>
-                                    <a href="view_photo.php/?id=<?php echo $photo->photo_id; ?>">View</a>
+                                    <a href="delete_photo.php/?photo_id=<?php echo $photo->photo_id;?>">Delete</a>
+                                    <a href="edit_photo.php/?photo_id=<?php echo $photo->photo_id; ?>">Edit</a>
+                                    <a href="view_photo.php/?photo_id=<?php echo $photo->photo_id; ?>">View</a>
                                 </div>
                             </td>
                             <td><?php echo $photo->photo_id; ?></td>
