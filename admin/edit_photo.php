b4e942104830299422f6448267c4e14e53d6ddbc
wcalvert88 <wcalvert88@gmail.com>
2018-04-25 14:56:21 -0400

    finished form for edit_photo.php and tested update button

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -3,7 +3,10 @@ if(!$session->is_signed_in()) {
     redirect("login.php");
 }
 
-$photos = Photo::find_all();
+if(isset($_POST['update'])) {
+    echo "Yes it works";
+}
+// $photos = Photo::find_all();
 ?>
 
     <!-- Navigation -->
@@ -33,6 +36,7 @@ $photos = Photo::find_all();
                 Edit Photos
                 <small>Subheading</small>
             </h1>
+            <form action="edit_photo.php" method="post">
             <div class="col-md-8">
                 <div class="form-group">
                     <input type="text" name="title" class="form-control">
@@ -55,7 +59,43 @@ $photos = Photo::find_all();
                 </div>
 
             </div>
+
+            <div class="col-md-4" >
+                <div  class="photo-info-box">
+                    <div class="info-box-header">
+                        <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                    </div>
+                <div class="inside">
+                    <div class="box-inner">
+                        <p class="text">
+                        <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                        </p>
+                        <p class="text ">
+                        Photo Id: <span class="data photo_id_box">34</span>
+                        </p>
+                        <p class="text">
+                        Filename: <span class="data">image.jpg</span>
+                        </p>
+                        <p class="text">
+                        File Type: <span class="data">JPG</span>
+                        </p>
+                        <p class="text">
+                        File Size: <span class="data">3245345</span>
+                        </p>
+                    </div>
+                    <div class="info-box-footer clearfix">
+                    <div class="info-box-delete pull-left">
+                        <a  href="delete_photo.php?id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg ">Delete</a>   
+                    </div>
+                    <div class="info-box-update pull-right ">
+                        <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                    </div>   
+                    </div>
+                </div>          
+            </div>
+            </form>            
         </div>
+
     </div>
     <!-- /.row -->
 
