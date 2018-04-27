a9df391264af980589bfc1e31e59a7781ec95e6d
wcalvert88 <wcalvert88@gmail.com>
2018-04-27 10:07:47 -0400

    installed a new text editor for textarea I will be changing after the course though because of licensing notifications

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+  <!-- WYSIWYG -->
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
