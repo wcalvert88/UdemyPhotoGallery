67d9a23fc6c25bc8f40a3074e84afa0a61886a21
wcalvert88 <wcalvert88@gmail.com>
2018-04-27 10:27:42 -0400

    built users page

--- admin/users.php
+++ admin/users.php
@@ -1,4 +1,10 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/header.php");
+if(!$session->is_signed_in()) {
+    redirect("login.php");
+}
+
+$users = User::find_all();
+?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -27,14 +33,42 @@
                 Users
                 <small>Subheading</small>
             </h1>
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> Blank Page
-                </li>
-            </ol>
+            <div class="col-md-12">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Photo</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        foreach ($users as $user) : ?>
+                        <tr>
+                            <td><?php echo $user->id; ?></td>
+                            <td>
+                                <img class="admin-user-thumbnail" src="<?php echo $user->user_image; ?>" alt="" width="200px">
+                            </td>
+                            <td><?php echo $user->username; ?>                                <div class="action_links">
+                                    <a href="delete_user.php?id=<?php echo $user->id;?>">Delete</a>
+                                    <a href="edit_user.php?id=<?php echo $user->id; ?>">Edit</a>
+                                    <a href="view_user.php?id=<?php echo $user->id; ?>">View</a>
+                                </div></td>
+                            <td><?php echo $user->first_name; ?></td>
+                            <td><?php echo $user->last_name; ?></td>
+                        </tr>
+
+                        <?php endforeach; ?>
+                    </tbody>   
+                </table>
+
+            
+            
+            
+            </div>
         </div>
     </div>
     <!-- /.row -->
