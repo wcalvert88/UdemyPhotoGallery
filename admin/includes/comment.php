180c66300377edca87e0132c44c320d0953bca1c
wcalvert88 <wcalvert88@gmail.com>
2018-04-29 17:52:38 -0400

    instantiated the photo comment class

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -8,6 +8,19 @@ class Commment extends Db_object {
     public $photo_id;
     public $author;
     public $body;
+
+    public static function create_comment($photo_id, $author="John Doe", $body="") {
+        if(!empty($photo_id) && !empty($author) && !empty($body)) {
+            $comment = new Comment();
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment-body = $body;
+
+            return $comment;
+        } else {
+            return false;
+        }
+    }
  
 
 
