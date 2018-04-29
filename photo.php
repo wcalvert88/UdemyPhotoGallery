4c837a2fc9a7c500e2775a05784ead056e2b30c8
wcalvert88 <wcalvert88@gmail.com>
2018-04-29 18:32:22 -0400

    customized the comment form in photo.php

--- photo.php
+++ photo.php
@@ -1,3 +1,11 @@
+<?php 
+
+if(isset($_POST['submit'])) {
+    echo "Hello";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -101,21 +109,38 @@
 
                 <!-- Blog Comments -->
 
+
+
+
+
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                    <form role="form" method="post">
+                    <div class="form-group">
+                        <label for="author">Author</label>
+                            <input type="text" name="author" class="form-control">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3" name="body"></textarea>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
+
+
+
+
+
+
 
                 <hr>
 
                 <!-- Posted Comments -->
 
+
+
+
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
@@ -129,31 +154,7 @@
                     </div>
                 </div>
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
+                
 
             </div>
 
