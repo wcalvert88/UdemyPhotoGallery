eff2c47c75992910e16b049801764bf491145f79
wcalvert88 <wcalvert88@gmail.com>
2018-04-30 08:24:19 -0400

    made comment page in admin

--- admin/comments.php
+++ admin/comments.php
@@ -1,4 +1,10 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/header.php");
+if(!$session->is_signed_in()) {
+    redirect("login.php");
+}
+
+$comments = Comment::find_all();
+?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -25,16 +31,37 @@
         <div class="col-lg-12">
             <h1 class="page-header">
                 Comments
-                <small>Subheading</small>
+                <a href="add_comment.php" class="btn btn-primary">Add Comment</a>
             </h1>
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> Blank Page
-                </li>
-            </ol>
+            <div class="col-md-12">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Author</th>
+                            <th>Body</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        foreach ($comments as $comment) : ?>
+                        <tr>
+                            <td><?php echo $comment->id; ?></td>
+                            
+                            <td><?php echo $comment->author; ?>                                <div class="action_links">
+                                    <a href="delete_comment.php?id=<?php echo $comment->id;?>">Delete</a>
+                                </div></td>
+                            <td><?php echo $comment->body; ?></td>
+                        </tr>
+
+                        <?php endforeach; ?>
+                    </tbody>   
+                </table>
+
+            
+            
+            
+            </div>
         </div>
     </div>
     <!-- /.row -->
