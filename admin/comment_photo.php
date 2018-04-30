8bf3a6770a47a1ca474ff804cf29e348793a736d
wcalvert88 <wcalvert88@gmail.com>
2018-04-30 15:27:02 -0400

    made photo.php dynamic

--- admin/comment_photo.php
+++ admin/comment_photo.php
@@ -38,7 +38,7 @@ $comments = Comment::find_comments($_GET['id']);
         <div class="col-lg-12">
             <h1 class="page-header">
                 Comments
-                <a href="add_comment.php" class="btn btn-primary">Add Comment</a>
+
             </h1>
             <div class="col-md-12">
                 <table class="table table-hover">
