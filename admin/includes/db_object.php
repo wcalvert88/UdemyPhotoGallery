8e371b862e3980ce6971c8131f543007c8c2075c
wcalvert88 <wcalvert88@gmail.com>
2018-04-30 21:16:55 -0400

    made counter to count all of any database for totals of photos users and comments

--- admin/includes/db_object.php
+++ admin/includes/db_object.php
@@ -142,7 +142,17 @@ class Db_object {
         return (mysqli_affected_rows($database->connection) == 1) ? true : false;
     } // End Delete Method
 
-}
+
+    public static function count_all() {
+        global $database;
+        $sql = "SELECT COUNT(*) FROM " . static::$db_table;;
+        $result_set = $database->query($sql);
+        $row = mysqli_fetch_array($result_set);
+
+        return array_shift($row);
+    }
+
+} // End Class Db_object
 
 
 
