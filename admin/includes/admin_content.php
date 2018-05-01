bbe181150b9520928ea3e2d368193d4ec6ba2aa9
wcalvert88 <wcalvert88@gmail.com>
2018-04-30 20:53:47 -0400

    setup the dashboard

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,77 +8,108 @@
                 <small>Subheading</small>
             </h1>
             
-            <?php 
+            <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-users fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">4</div>
+                                        <div>New Views</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                  <span class="pull-left">View Details</span> 
+                               <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span> 
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
-            // $user = User::find_by_id(11);
-            // echo $user->username;
+                     <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-photo fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">7</div>
+                                        <div>Photos</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Photos in Gallery</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
-            // $photo = Photo::find_by_id(1);
-            // echo $photo->filename;
-            // $user = new User();
-            // $user->username = "SuaveTheThird12";
-            // $user->password = "password";
-            // $user->first_name = "Rick";
-            // $user->last_name = "Suavo";
-        
-            // $user->create();
 
-            // $user = User::find_user_by_id(11);
-            // $user->username = "Williamson2";
-            // $user->password = "Williamson32";
-            // $user->first_name = "Williamson43";
-            // $user->last_name = "Williamson45";
+                     <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">8
 
-            // $user->update();
+                                        </div>
 
-            // $user = User::find_user_by_id(3);
-            // $user->delete();
+                                        <div>Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Users</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                      <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-support fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">8</div>
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Comments</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+
+                        </div> <!--First Row-->
+
+
             
-            // $user = User::find_user_by_id(8);
-            // $user->password = "justPassword";
-            // $user->save();
-
-            // $user = new User();
-            // $user->username = "NEW_USER";
-            // $user->save();
-
-            // $user = User::find_user_by_id(8);
-            // $user->delete();
-
-            // $users = User::find_all();
-
-            // foreach ($users as $user) {
-            //     echo $user->username;
-            // }
-            
-            // $photos = Photo::find_all();
-
-            // foreach ($photos as $photo) {
-            //     echo $photo->title;
-            // }
-
-            // $photos = new Photo();
-            // $photos->title = "Just some text";
-            // $photos->size = 20;
-            // $photos->picture_path("C:\MAMP\htdocs\UdemyPhotoGalleryDownload\car_pictures\_large_image_1.jpg");
-            // $photos->save();
-            // $photos->create();
-            // $photos = new Photo();
-            // $photos->title = "Just some text";
-            // $photos->size = 20;
-        
-            // $photos->create();
-
-            // echo INCLUDES_PATH;
-            ?>
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                </li>
-                <li class="active">
-                    <i class="fa fa-file"></i> Blank Page
-                </li>
-            </ol>
         </div>
     </div>
     <!-- /.row -->
