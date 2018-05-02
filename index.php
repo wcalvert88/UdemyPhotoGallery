d80b8610cc361aeeff6e14d9991329a161de9f8f
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 19:17:55 -0400

    added multiple button pagination show

--- index.php
+++ index.php
@@ -41,10 +41,20 @@ $photos = Photo::find_by_query($sql);
 
                         <?php if ($paginate->page_total() > 1) {
                                 if ($paginate->has_next()) {
-                                        echo "<li class='next'><a href=''>Next</a></li>";
+                                        echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
                                 }
+
+                                for ($i=1; $i <= $paginate->page_total(); $i++) { 
+                                        if ($i == $paginate->current_page) {
+                                                echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                                        } else {
+                                                echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";  
+                                        }
+                                        
+                                }
+                                
                                 if ($paginate->has_previous()) {
-                                        echo "<li class='previous'><a href=''>Previous</a></li>";
+                                        echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
                                 }
 
                         }
