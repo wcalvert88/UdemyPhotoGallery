306add2c5312435f1499413293dfcd43b1b3cb44
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 15:45:32 -0400

    started working on front-end index.php

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/header.php"); 
+$photos = Photo::find_all();
+
+?>
 
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
 
-    
-            
+                <?php foreach ($photos as $photo): ?>
+                
+                <div class="thumbnails row">
+                        <div class="col-xs-6 col-md-3">
+                        <a class="thumbnail" href="">
+                        <img src="" alt="">                                
+
+                        </a>
+                        
+                        </div>
+                
+                </div>
+
+
+                <?php endforeach; ?>
           
          
 
@@ -16,11 +32,11 @@
 
 
 
-            <!-- Blog Sidebar Widgets Column -->
+            <!-- Blog Sidebar Widgets Column
             <div class="col-md-4">
 
             
-                 <?php include("includes/sidebar.php"); ?>
+                 <?php //include("includes/sidebar.php"); ?> -->
 
 
 
