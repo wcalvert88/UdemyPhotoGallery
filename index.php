d1990bb3907a56947b431d69bcce7d2ffc66abeb
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 16:46:20 -0400

    started to setup pagination

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php include("includes/header.php"); 
-$photos = Photo::find_all();
 
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+
+$items_per_page = 4;
+
+$items_total_count = Photo::count_all();
+
+
+
+$photos = Photo::find_all();
 ?>
 
 
