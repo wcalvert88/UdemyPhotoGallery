a86f2e64bc4a5f72384a9464d830aff338caa0a3
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 12:27:04 -0400

    started to get pie chart working

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -20,15 +20,19 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
+          ['Views',     <?php echo $session->count; ?>],
+          ['Photos',      2],
           ['Commute',  2],
           ['Watch TV', 2],
           ['Sleep',    7]
         ]);
 
         var options = {
-          title: 'My Daily Activities'
+          legend:'none',
+          pieSliceText: 'label',
+          backgroundColor: 'transparent'
+          title: 'My Daily Activities',
+
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
