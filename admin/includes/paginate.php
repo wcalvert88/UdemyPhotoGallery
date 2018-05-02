09569f0f72bd5e9a95c012e33f19e7784f278dbb
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 16:48:55 -0400

    started Paginate Class

---
+++ admin/includes/paginate.php
@@ -0,0 +1,23 @@
+<?php 
+class Paginate {
+    public $page;
+    public $items_per_page;
+    public $items_total_count;
+
+    public function __construct($page = 1, $items_per_page=4, $items_total_count=0) {
+
+    }
+
+} // End Class Paginate
+
+
+
+
+
+
+
+
+
+
+
+?>
