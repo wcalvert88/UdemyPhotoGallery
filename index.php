80873c32409e04cf32042ecd9c2a9481e49c0253
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 16:11:34 -0400

    styled the pictures on the homepage

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $photos = Photo::find_all();
  
                         <div class="col-xs-6 col-md-3">
                         <a class="thumbnail" href="">
-                        <img src="admin/<?php echo $photo->picture_path(); ?>" alt="">                                
+                        <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picture_path(); ?>" alt="">                                
 
                         </a>
                         
