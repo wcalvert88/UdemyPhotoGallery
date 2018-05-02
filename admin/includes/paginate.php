44997118ded0a73df75763914bcf6d8e06d8366a
wcalvert88 <wcalvert88@gmail.com>
2018-05-02 16:51:24 -0400

    finished __construct of paginate class

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -1,12 +1,14 @@
 <?php 
 class Paginate {
-    public $page;
+    public $current_page;
     public $items_per_page;
     public $items_total_count;
 
-    public function __construct($page = 1, $items_per_page=4, $items_total_count=0) {
-
-    }
+    public function __construct($current_page = 1, $items_per_page=4, $items_total_count=0) {
+        $this->current_page = (int)$page;
+        $this->items_per_page = (int)$items_per_page;
+        $this->items_total_count = (int)$items_total_count;
+    } // End __construct method
 
 } // End Class Paginate
 
