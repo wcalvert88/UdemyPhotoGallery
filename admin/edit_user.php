7550086c38605e33e640e8bfaf64c4e7ad7e5e47
wcalvert88 <wcalvert88@gmail.com>
2018-05-03 10:06:16 -0400

    got the user id in jquery

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -100,7 +100,7 @@ include ("includes/photo_library_modal.php");
                     </div>
 
                     <div class="form-group">
-                        <a class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                        <a id="user-id" class="btn btn-danger" href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
                         <input type="submit" name="update" class="btn btn-primary" value="Update">
                     </div>
 
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-
+    var user_href;
+    var user_href_split;
+    var user_id;
     $(".modal_thumbnails").click(function () {
         $("#set_user_image").prop('disabled', false);
     });
 
-
-
+    user_href = $("#user-id").prop('href');
+    user_href_split = user_href.split("=");
+    user_id = user_href_split[user_href_split.length - 1];
+    alert(user_id);
 
 
 tinymce.init({ selector:'textarea' });
