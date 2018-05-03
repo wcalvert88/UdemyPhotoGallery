45130f1b1da51ccd366c87960110e5aa8a6fc813
wcalvert88 <wcalvert88@gmail.com>
2018-05-03 10:26:13 -0400

    set up ajax request to get picture from database

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     var user_id;
     var image_src;
     var image_src_split;
-    var image_id;
+    var image_name;
     $(".modal_thumbnails").click(function () {
         $("#set_user_image").prop('disabled', false);
-    });
+ 
 
     user_href = $("#user-id").prop('href');
     user_href_split = user_href.split("=");
@@ -15,8 +15,22 @@ $(document).ready(function() {
     
     image_src = $(this).prop("src");
     image_src_split = image_src.split("/");
-    image_id = image_src_split[image_src_split.length - 1];
-    alert(image_id);
+    image_name = image_src_split[image_src_split.length - 1];
+    });
+
+    $("#set_user_image").click(function() {
+        $.ajax({
+            url: "includes/ajax_code.php",
+            data: {image_name: image_name, user_id: user_id},
+            type: "POST",
+            success: function(data) {
+                if(!data.error) {
+                    alert(image_name);
+                }
+            }
+        });
+
+    });
 
 tinymce.init({ selector:'textarea' });
 });
