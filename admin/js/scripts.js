687adadac07573a5464aed939860c7dc879317f5
wcalvert88 <wcalvert88@gmail.com>
2018-05-03 10:13:35 -0400

    got the photo id clicked on

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     var user_href;
     var user_href_split;
     var user_id;
+    var image_src;
+    var image_src_split;
+    var image_id;
     $(".modal_thumbnails").click(function () {
         $("#set_user_image").prop('disabled', false);
     });
@@ -9,8 +12,11 @@ $(document).ready(function() {
     user_href = $("#user-id").prop('href');
     user_href_split = user_href.split("=");
     user_id = user_href_split[user_href_split.length - 1];
-    alert(user_id);
-
+    
+    image_src = $(this).prop("src");
+    image_src_split = image_src.split("/");
+    image_id = image_src_split[image_src_split.length - 1];
+    alert(image_id);
 
 tinymce.init({ selector:'textarea' });
 });
