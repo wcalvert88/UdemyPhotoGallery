9214ff0a28a3bef8cd0ec8eed565c3884839e182
wcalvert88 <wcalvert88@gmail.com>
2018-05-03 09:52:09 -0400

    got modal working and made it more mobile friendly

--- admin/includes/photo_library_modal.php
+++ admin/includes/photo_library_modal.php
@@ -1,3 +1,10 @@
+<?php require_once("init.php"); 
+
+$photos = Photo::find_all();
+
+?>
+
+
 <div class="modal fade" id="photo-library">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -9,16 +16,16 @@
           <div class="col-md-9">
              <div class="thumbnails row">
             
-                <!-- PHP LOOP HERE CODE HERE-->
+                <?php foreach($photos as $photo): ?>
                 
-               <div class="col-xs-2">
+               <div class="col-xs-4">
                  <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
-                   <img class="modal_thumbnails img-responsive" src="<!-- PHP LOOP HERE CODE HERE-->" data="<!-- PHP LOOP HERE CODE HERE-->">
+                   <img class="modal_thumbnails img-responsive" src="<?php echo $photo->picture_path(); ?>" data="<!-- PHP LOOP HERE CODE HERE-->">
                  </a>
                   <div class="photo-id hidden"></div>
                </div>
-
-                    <!-- PHP LOOP HERE CODE HERE-->
+                <?php endforeach; ?>
+                    
 
              </div>
           </div><!--col-md-9 -->
